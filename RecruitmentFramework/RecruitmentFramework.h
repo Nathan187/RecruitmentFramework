222d722b87d3780dd1cd8791d347ff3a2d7267fe
nathan <nathan_boyd@oxy.com>
2018-03-17 19:38:45 -0500

    initial

---
+++ RecruitmentFramework/RecruitmentFramework.h
@@ -0,0 +1,19 @@
+//
+//  RecruitmentFramework.h
+//  RecruitmentFramework
+//
+//  Created by Nathan on 3/17/18.
+//  Copyright © 2018 Oxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RecruitmentFramework.
+FOUNDATION_EXPORT double RecruitmentFrameworkVersionNumber;
+
+//! Project version string for RecruitmentFramework.
+FOUNDATION_EXPORT const unsigned char RecruitmentFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecruitmentFramework/PublicHeader.h>
+
+
