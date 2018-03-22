dac86d42c8cd0bae89c753bf8111361d51093848
nathan <nathan_boyd@oxy.com>
2018-03-22 09:31:44 -0500

    checking in before implementing getting token

--- Authenticate View/AuthenticateViewController.swift
+++ Authenticate View/AuthenticateViewController.swift
@@ -1,12 +1,6 @@
-//
-//  AuthenticateViewController.swift
-//  RecruitmentFramework
-//
-//  Created by mobiledeveloper on 3/20/18.
-//  Copyright © 2018 Oxy. All rights reserved.
-//
-
+import Foundation
 import UIKit
+import BonMot
 
 public protocol AuthenticateViewControllerDelegate : class  {
     func AddRemoveCredentials(store : Bool, username : String, password : String)
@@ -26,6 +20,16 @@ public class AuthenticateViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var itms : [Composable] = []
+        let styleHeader = StringStyle(.font(UIFont(name: "AvenirNext-Bold", size: 14)!), .color(.black))
+        let styleDetail = StringStyle(.font(UIFont(name: "AvenirNext-Medium", size: 12.5)!), .color(.black))
+        
+        itms.append("Authenticate\n\n".styled(with: styleHeader))
+        itms.append("Please enter your network credentials before proceeding.  This operation is idempotent.  You can perform a sync repeatedly.".styled(with: styleDetail))
+        
+        lblTitle.attributedText = NSAttributedString.composed(of: itms)
+        lblSaveCredentials.attributedText = "Remember Credentials".styled(with: styleDetail)
+         
     }
 
     public override func didReceiveMemoryWarning() {
@@ -40,7 +44,16 @@ public class AuthenticateViewController: UIViewController {
     public func set(password text: String) {
         self.txtPassword.text = text
     }
+    
+    public func set(message attrString: NSAttributedString)
+    {
+        self.lblTitle.attributedText = attrString
+    }
 
+    public func set(messageBySwitch attrString : NSAttributedString)
+    {
+        self.lblSaveCredentials.attributedText = attrString
+    }
     /*
     // MARK: - Navigation
 
